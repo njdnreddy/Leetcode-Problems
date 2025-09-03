8de63ffa1bc3b4a3bef0577722779f8c8e8fefed
Navya Reddy Chinnamallaiahgari <147297807+njdnreddy@users.noreply.github.com>
2025-09-03 11:11:17 +0530

    Time: 1 ms (99.57%), Space: 43.1 MB (82.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,38 @@
 import java.util.*;
 class Solution {
     public boolean isAnagram(String s, String t) {
-        s=s.toLowerCase();
-        t=t.toLowerCase();
+        /*if(s.length()!=t.length()){
+            return false;
+        }
+        int[] check=new int[26];
+        char[] ch1=s.toCharArray();
+        char[] ch2=t.toCharArray();
+        for(int i=0;i<ch1.length;i++){
+            check[ch1[i]-'a']++;
+        }
+            for(int i=0;i<ch2.length;i++){
+                if(check[ch2[i]-'a']==0){
+                    return false;
+                }
+                check[ch2[i]-'a']--;
+            }
+            return true;*/
         if(s.length()!=t.length()){
             return false;
         }
-        char[] ch=s.toCharArray();
-        char[] ch1=t.toCharArray();
-        Arrays.sort(ch);
-        Arrays.sort(ch1);
-        return Arrays.equals(ch,ch1);
+        int[] count=new int[26];
+        for(char c:s.toCharArray()){
+            count[c-'a']++;
+        }
+        for(char c:t.toCharArray()){
+            count[c - 'a']--;
+        }
+        for(int val:count){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
     
