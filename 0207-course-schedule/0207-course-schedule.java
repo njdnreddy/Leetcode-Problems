8fcacf52ab3a6c77d1857ba03b9e0a3ceeab39b5
Navya Reddy Chinnamallaiahgari <147297807+njdnreddy@users.noreply.github.com>
2025-09-08 20:55:07 +0530

    Time: 3 ms (99.67%), Space: 44.9 MB (94.81%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,42 @@
+class Solution {
+    List<Integer>[] adj;
+    boolean[] visited;
+    boolean[] marked;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        adj=new ArrayList[numCourses];
+        for(int i=0;i<numCourses;i++){
+            adj[i]=new ArrayList<>();
+        }
+        visited=new boolean[numCourses];
+        marked=new boolean[numCourses];
+        for(int i=0;i<prerequisites.length;i++){
+            adj[prerequisites[i][0]].add(prerequisites[i][1]);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(iscyclic(i)){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+    boolean iscyclic(int i){
+        visited[i]=true;
+        for(Integer j:adj[i]){
+            if(!visited[j]){
+                if(iscyclic(j)){
+                    return true;
+                }
+            }
+            else if(!marked[j]){
+                return true;
+            }
+        }
+        marked[i]=true;
+        return false;
+
+    }
+}
